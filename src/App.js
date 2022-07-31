5491a992665de564644cd4c08023b2bfff570ee0
Matthew Flanagan <mattflanagan40@gmail.com>
2022-07-31 13:02:55 -0400

    random names - handleNameChange function

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleNameChange = () => {
+    const names = ['Matt', 'Vito']
+    const int = Math.floor(Math.random() * 2);
+    return names[int];
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello {handleNameChange()}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
