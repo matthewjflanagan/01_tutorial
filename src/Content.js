6624012f70cd295dad4a057ec3a1e5ad5410a70b
Matthew Flanagan <mattflanagan40@gmail.com>
2022-07-31 13:38:24 -0400

    first click event - you clicked it button

--- src/Content.js
+++ src/Content.js
@@ -4,9 +4,16 @@ export const Content = () => {
         const int = Math.floor(Math.random() * 2);
         return names[int];
         }
+
+    const handleClick = () => {
+      console.log('you clicked it!')
+    }
   return (
     <main>
+      <p>
         Hello {handleNameChange()}!
+      </p>
+      <button onClick={handleClick}>Click it</button>
     </main>
   )
 }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,7 @@
 const Header = () => {
-    const headerStyles = {
-        backgroundColor: 'mediumblue', 
-        color: '#fff'
-    }
+
   return (
-    <header style={headerStyles}>
+    <header>
         <h1>Groceries List</h1>
     </header>
   )
